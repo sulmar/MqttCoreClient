25fb767ac2c201ca3b948efe2a3d9b48478af1a3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-09-13 21:43:18 +0200

    Init

--- MqttClient/Program.cs
+++ MqttClient/Program.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MqttClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("MQTT Publisher started!");
+
+            await ClientTestAsync();
+
+        }
+
+        // PM> Install-Package MQTTnet.AspNetCore
+        public static async Task ClientTestAsync()
+        {
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer("10.0.75.1", 1883) // Port is optional
+                .Build();
+
+            var factory = new MqttFactory();
+
+            var client = factory.CreateMqttClient();
+
+            await client.ConnectAsync(options);
+
+            Random random = new Random();
+
+            Console.WriteLine("Press Esc key to exit.");
+
+            while (true)
+            {
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape) break;
+
+                string payload = $"{random.Next(0, 100)}";
+
+                await client.PublishAsync("house/room1/temp1", payload);
+
+                Console.WriteLine($"Sent {payload}");
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+            
+
+            //client.ApplicationMessageReceived += (s, e) =>
+            //{
+            //    Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+            //    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+            //    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            //    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+            //    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+            //    Console.WriteLine();
+
+            //};
+
+            //client.Connected += async (s, e) =>
+            //{
+            //    Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+            //    // new TopicFilterBuilder().WithTopic("house/#").Build()
+            //    await client.SubscribeAsync("house/#");
+
+            //    Console.WriteLine("### SUBSCRIBED ###");
+            //};
+
+
         }
     }
 }
---
+++ MqttSubsciber/Program.cs
@@ -0,0 +1,74 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttSubsciber
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            await ClientTestAsync();
+
+            Console.WriteLine("Press any key to exit.");
+
+            Console.ReadKey();
+        }
+
+        // PM> Install-Package MQTTnet.AspNetCore
+        public static async Task ClientTestAsync()
+        {
+            
+
+            var factory = new MqttFactory();
+
+            var client = factory.CreateMqttClient();
+
+            // await client.SubscribeAsync("house#");
+
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer("10.0.75.1", 1883) // Port is optional
+                .Build();
+
+            client.ApplicationMessageReceived += Client_ApplicationMessageReceived;
+            client.Connected += Client_Connected;
+            client.Disconnected += Client_Disconnected;
+
+            await client.ConnectAsync(options);
+
+            
+
+        }
+
+        private static async void Client_Connected(object sender, MqttClientConnectedEventArgs e)
+        {
+            Console.WriteLine("# Connected to server." );
+
+            IMqttClient client = (IMqttClient)sender;
+
+            // Subscribe to a topic
+            await client.SubscribeAsync(new TopicFilterBuilder().WithTopic("house/#").Build());
+
+            Console.WriteLine("Subscribed.");
+        }
+
+        private static void Client_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+            Console.WriteLine("# Recevied message");
+            Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+            Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+            Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+            Console.WriteLine();
+        }
+
+        private static void Client_Disconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            
+        }
+    }
+}
